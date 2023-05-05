6fce29a63fc22cc169f2ed844de49345989947ef
Juan <jpichardo369@gmail.com>
2023-05-05 13:44:17 -0400

    Convert to Spigot

--- src/main/kotlin/club/piglin/brimstone/Brimstone.kt
+++ src/main/kotlin/club/piglin/brimstone/Brimstone.kt
@@ -1,11 +1,28 @@
 package club.piglin.brimstone
 
-import net.fabricmc.api.ModInitializer
+import com.comphenix.protocol.ProtocolLibrary
+import com.comphenix.protocol.ProtocolManager
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Logger
 
-@Suppress("UNUSED")
-object Brimstone : ModInitializer {
-    private const val MOD_ID = "brimstone"
-    override fun onInitialize() {
-        println("Brimstone has been initialized.")
+class Brimstone : JavaPlugin() {
+    companion object {
+        @JvmStatic
+        lateinit var instance: Brimstone
+        lateinit var Log: Logger
+    }
+
+    lateinit var protocolManager: ProtocolManager
+
+    override fun onEnable() {
+        instance = this
+        Log = Bukkit.getLogger()
+        protocolManager = ProtocolLibrary.getProtocolManager()
+        Log.info("The plugin has successfully loaded.")
+    }
+
+    override fun onDisable() {
+        Log.info("The plugin has successfully unloaded.")
     }
 }
