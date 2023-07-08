0a8086b3f315d90d41cd3ee30fc29a566726739f
Juan <jpichardo369@gmail.com>
2023-07-07 23:05:35 -0400

    Adjust the EXP, actually TEST IT IN GAME to check it if feels good
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/club/piglin/brimstone/listeners/SkillListener.kt
+++ src/main/kotlin/club/piglin/brimstone/listeners/SkillListener.kt
@@ -4,11 +4,20 @@ import club.piglin.brimstone.Brimstone
 import club.piglin.brimstone.database.profiles.Skill
 import org.bukkit.Material
 import org.bukkit.block.data.Ageable
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.entity.EntityDeathEvent
 
 class SkillListener : Listener {
+    @EventHandler
+    fun onMobDeath(e: EntityDeathEvent) {
+        if (e.entity.killer is Player) {
+
+        }
+    }
+
     @EventHandler
     fun onFarmMine(e: BlockBreakEvent) {
         if (!e.isCancelled) {
@@ -18,19 +27,22 @@ class SkillListener : Listener {
                 if (blockData.maximumAge == blockData.age) {
                     when (e.block.type) {
                         Material.WHEAT -> {
-                            exp = 4.0
+                            exp = 7.5
                         }
                         Material.POTATOES -> {
-                            exp = 4.0
+                            exp = 7.5
                         }
                         Material.CARROTS -> {
-                            exp = 4.0
+                            exp = 7.5
                         }
                         Material.BEETROOTS -> {
-                            exp = 4.0
+                            exp = 7.5
                         }
                         Material.COCOA -> {
-                            exp = 5.0
+                            exp = 15.5
+                        }
+                        Material.PITCHER_CROP -> {
+                            exp = 125.5
                         }
                         else -> {}
                     }
@@ -49,17 +61,8 @@ class SkillListener : Listener {
                     Material.SUGAR_CANE -> {
                         exp = 0.45
                     }
-                    Material.PITCHER_CROP -> {
-                        exp = 5.5
-                    }
-                    Material.PITCHER_PLANT -> {
-                        exp = 5.5
-                    }
-                    Material.TORCHFLOWER_CROP -> {
-                        exp = 5.5
-                    }
                     Material.TORCHFLOWER -> {
-                        exp = 5.5
+                        exp = 125.0
                     }
 
                     else -> {}
@@ -76,31 +79,31 @@ class SkillListener : Listener {
             var exp = 0.0
             when (e.block.type) {
                 Material.ANCIENT_DEBRIS -> {
-                    exp = 35.0
+                    exp = 200.0
                 }
                 Material.DIAMOND_ORE, Material.DEEPSLATE_DIAMOND_ORE -> {
-                    exp = 25.0
+                    exp = 125.0
                 }
                 Material.EMERALD_ORE, Material.DEEPSLATE_EMERALD_ORE -> {
-                    exp = 15.0
+                    exp = 75.0
                 }
                 Material.GOLD_ORE, Material.DEEPSLATE_GOLD_ORE, Material.NETHER_GOLD_ORE -> {
-                    exp = 15.0
+                    exp = 50.0
                 }
                 Material.LAPIS_ORE, Material.DEEPSLATE_LAPIS_ORE, Material.NETHER_QUARTZ_ORE -> {
-                    exp = 12.5
+                    exp = 35.5
                 }
                 Material.REDSTONE_ORE, Material.DEEPSLATE_REDSTONE_ORE -> {
-                    exp = 7.5
+                    exp = 25.5
                 }
                 Material.COAL_ORE, Material.DEEPSLATE_COAL_ORE, Material.COPPER_ORE -> {
-                    exp = 1.5
+                    exp = 12.5
                 }
                 Material.IRON_ORE, Material.DEEPSLATE_IRON_ORE -> {
-                    exp = 2.5
+                    exp = 35.5
                 }
                 Material.STONE, Material.ANDESITE, Material.GRANITE, Material.DIORITE, Material.DEEPSLATE -> {
-                    exp = 0.5
+                    exp = 1.5
                 }
                 else -> {}
             }
