839400ebf62ae92206deb3a12471acc6ace6bbf0
Juan <jpichardo369@gmail.com>
2023-07-12 17:32:46 -0400

    More work on the shop
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/club/piglin/brimstone/commands/menus/ShopGUI.kt
+++ src/main/kotlin/club/piglin/brimstone/commands/menus/ShopGUI.kt
@@ -1,7 +1,9 @@
 package club.piglin.brimstone.commands.menus
 
+import club.piglin.brimstone.features.ShopCategory
 import club.piglin.brimstone.menus.Button
 import club.piglin.brimstone.menus.Menu
+import club.piglin.brimstone.menus.pagination.PaginatedMenu
 import org.bukkit.entity.Player
 
 class ShopGUI : Menu() {
@@ -9,4 +11,8 @@ class ShopGUI : Menu() {
         val buttons = HashMap<Int, Button>()
         return buttons
     }
+}
+
+class ShopPage(category: ShopCategory) : PaginatedMenu() {
+
 }
--- src/main/kotlin/club/piglin/brimstone/features/Shop.kt
+++ src/main/kotlin/club/piglin/brimstone/features/Shop.kt
@@ -52,6 +52,29 @@ class ShopHandler {
     }
 
     init {
-
+        addEntry(Material.GRASS_BLOCK, ShopCategory.BUILDING_BLOCKS, 2.0, 0.5)
+        addEntry(Material.DIRT, ShopCategory.BUILDING_BLOCKS, 0.75, 0.1)
+        addEntry(Material.COBBLESTONE, ShopCategory.BUILDING_BLOCKS, 0.75, 0.1)
+        addEntry(Material.GRAVEL, ShopCategory.BUILDING_BLOCKS, 2.0, 0.5)
+        addEntry(Material.MOSSY_COBBLESTONE, ShopCategory.BUILDING_BLOCKS, 2.0, 0.5)
+        addEntry(Material.STONE, ShopCategory.BUILDING_BLOCKS, 0.75, 0.1)
+        addEntry(Material.GRANITE, ShopCategory.BUILDING_BLOCKS, 2.0, 0.5)
+        addEntry(Material.DIORITE, ShopCategory.BUILDING_BLOCKS, 1.5, 0.8)
+        addEntry(Material.ANDESITE, ShopCategory.BUILDING_BLOCKS, 0.75, 0.1)
+        addEntry(Material.OAK_LOG, ShopCategory.BUILDING_BLOCKS, 5.0, 3.0)
+        addEntry(Material.BIRCH_LOG, ShopCategory.BUILDING_BLOCKS, 5.0, 3.0)
+        addEntry(Material.JUNGLE_LOG, ShopCategory.BUILDING_BLOCKS, 5.0, 3.0)
+        addEntry(Material.ACACIA_LOG, ShopCategory.BUILDING_BLOCKS, 5.0, 3.0)
+        addEntry(Material.DARK_OAK_LOG, ShopCategory.BUILDING_BLOCKS, 5.0, 3.0)
+        addEntry(Material.SNOW_BLOCK, ShopCategory.BUILDING_BLOCKS, 1.5, 0.01)
+        addEntry(Material.ICE, ShopCategory.BUILDING_BLOCKS, 3.0, 0.5)
+        addEntry(Material.SAND, ShopCategory.BUILDING_BLOCKS, 3.0, 0.5)
+        addEntry(Material.RED_SAND, ShopCategory.BUILDING_BLOCKS, 3.0, 0.5)
+        addEntry(Material.RED_SANDSTONE, ShopCategory.BUILDING_BLOCKS, 3.0, 0.5)
+        addEntry(Material.SANDSTONE, ShopCategory.BUILDING_BLOCKS, 3.0, 0.5)
+        addEntry(Material.GLASS, ShopCategory.BUILDING_BLOCKS, 5.0, 1.0)
+        addEntry(Material.CLAY, ShopCategory.BUILDING_BLOCKS, 12.0, 2.0)
+        addEntry(Material.TERRACOTTA, ShopCategory.BUILDING_BLOCKS, 15.0, 5.0)
+        addEntry(Material.BRICKS, ShopCategory.BUILDING_BLOCKS, 20.0, 8.0)
     }
 }
