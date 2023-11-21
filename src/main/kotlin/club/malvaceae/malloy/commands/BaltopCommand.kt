c59a786405fa792cb624784c3c776832a96a7105
Juan <jpichardo369@gmail.com>
2023-11-21 16:06:09 -0500

    Add a filter to /baltop so it doesn't include profiles w/ names that don't exist
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/club/malvaceae/malloy/commands/BaltopCommand.kt
+++ src/main/kotlin/club/malvaceae/malloy/commands/BaltopCommand.kt
@@ -2,6 +2,7 @@ package club.malvaceae.malloy.commands
 
 import club.malvaceae.malloy.Malloy
 import club.malvaceae.malloy.utils.Chat
+import com.mongodb.client.model.Filters
 import com.mongodb.client.model.Sorts
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
@@ -13,7 +14,8 @@ class BaltopCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         Chat.sendComponent(sender, "<st><dark_gray>                    </dark_gray></st> <gold><bold>Top Balances</bold></gold> <st><dark_gray>                    </dark_gray></st>")
         with (Malloy.instance.dataSource.getDatabase("malloy").getCollection("profiles")) {
-            val top = this.find().sort(Sorts.descending("gold")).limit(10)
+            val filter = Filters.ne("name", null)
+            val top = this.find(filter).sort(Sorts.descending("gold")).limit(10)
             for ((index, document) in top.withIndex()) {
                 if (document != null && document["uuid"] != null) {
                     val profile = Malloy.instance.profileHandler.lookupProfile(document["uuid"] as UUID).get()
