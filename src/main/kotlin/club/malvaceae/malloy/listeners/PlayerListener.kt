bed9782dd702e7571272df33a93aa3c4203efaed
Juan <jpichardo369@gmail.com>
2023-12-05 16:26:46 -0500

    Make newly joined players teleport to spawn
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/club/malvaceae/malloy/listeners/PlayerListener.kt
+++ src/main/kotlin/club/malvaceae/malloy/listeners/PlayerListener.kt
@@ -2,13 +2,17 @@ package club.malvaceae.malloy.listeners
 
 import club.malvaceae.malloy.Malloy
 import club.malvaceae.malloy.database.towns.Town
+import club.malvaceae.malloy.utils.Settings
 import com.vexsoftware.votifier.model.Vote
 import com.vexsoftware.votifier.model.VotifierEvent
 import io.papermc.paper.event.player.AsyncChatEvent
+import me.lucko.helper.Schedulers
 import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.milkbowl.vault.chat.Chat
 import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.OfflinePlayer
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -25,6 +29,11 @@ class PlayerListener : Listener {
     fun onPlayerJoin(e: PlayerJoinEvent) {
         val group: String? = vaultChat?.getPrimaryGroup(e.player)
         val prefix = vaultChat?.getGroupPrefix(e.player.world, group)
+        if ((e.player as OfflinePlayer).hasPlayedBefore()) {
+            Schedulers.sync().runLater({
+                e.player.teleportAsync(Location(Bukkit.getWorld("world"), Settings.data.getDouble("spawn.x"), Settings.data.getDouble("spawn.y"), Settings.data.getDouble("spawn.z"), Settings.data.getDouble("spawn.yaw").toFloat(), Settings.data.getDouble("spawn.pitch").toFloat()))
+            }, 20L)
+        }
         e.joinMessage(MiniMessage.miniMessage().deserialize("<dark_gray>[</dark_gray><green>+<dark_gray>]</dark_gray> <color:#28cf1f>${prefix}${e.player.name}"))
         club.malvaceae.malloy.utils.Chat.sendComponent(e.player, "<gray>Warning: This server is in <red>ALPHA</red>, many features may be added/removed later on.")
     }
